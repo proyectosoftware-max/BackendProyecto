77f209a5a73fd3a70cae1e03082b2d1f30b20076
Usuario <usuario@gmail.com>
2024-09-01 19:12:33 -0500

    Proyecto

--- modelo/conexion.js
+++ modelo/conexion.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // 30 segundos de timeout para seleccionar un servidor
+    socketTimeoutMS: 45000, // 45 segundos de timeout para operaciones socket
 }).then(()=>{
     console.log("base de datos iniciada");
 }).catch(err =>{
