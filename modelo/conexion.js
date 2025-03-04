672f7824f87a604dfd610cab34a05039999c4d85
Usuario <usuario@gmail.com>
2025-03-04 18:15:01 -0500

    Proyecto

--- modelo/conexion.js
+++ modelo/conexion.js
@@ -1,3 +1,16 @@
+/*const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // 30 segundos de timeout para seleccionar un servidor
+    socketTimeoutMS: 45000, // 45 segundos de timeout para operaciones socket
+}).then(()=>{
+    console.log("base de datos iniciada");
+}).catch(err =>{
+    console.log(err)
+})*/
+
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI,{
